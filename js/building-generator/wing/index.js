0e11fa67b6e764597ce2cad42b3e9f5a11457bb9
George Hinchliffe <george@georgehinch.com>
2026-08-13 13:47:33 -0700

    Use trim-aware wing generation for issue 232

--- js/building-generator/wing/index.js
+++ js/building-generator/wing/index.js
@@ -1,6 +1,6 @@
 import * as defaultWingTemplate from './default-wing-template.js';
 import * as blockContext from './block-context.js';
-import * as mainBlock from './main-block.js';
+import * as mainBlock from './trimmed-wing-generation.js';
 import * as wingSystem from './wing-system.js';
 
 export const wingModules = Object.freeze({
